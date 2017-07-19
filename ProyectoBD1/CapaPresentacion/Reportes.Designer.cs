c0b5e3909584e084922b74189637185797dc9b54
Alfredo <Alfredo@VALVERDE>
2017-07-18 22:06:45 -0600

    FrmReportes

--- ProyectoBD1/CapaPresentacion/Reportes.Designer.cs
+++ ProyectoBD1/CapaPresentacion/Reportes.Designer.cs
@@ -28,11 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtidsala = new System.Windows.Forms.TextBox();
+            this.txtidcartelera = new System.Windows.Forms.TextBox();
+            this.txtidpelicula = new System.Windows.Forms.TextBox();
+            this.txtventastotales = new System.Windows.Forms.TextBox();
+            this.btnventfuncion = new System.Windows.Forms.Button();
+            this.btnventaspelicula = new System.Windows.Forms.Button();
+            this.btnventastotales = new System.Windows.Forms.Button();
+            this.btnventaslapso = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtfechaini = new System.Windows.Forms.TextBox();
+            this.txtfechafin = new System.Windows.Forms.TextBox();
+            this.txttiquetes = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Sala";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID Cartelera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID Pelicula";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ventas Totales";
+            // 
+            // txtidsala
+            // 
+            this.txtidsala.Location = new System.Drawing.Point(143, 22);
+            this.txtidsala.Name = "txtidsala";
+            this.txtidsala.Size = new System.Drawing.Size(100, 20);
+            this.txtidsala.TabIndex = 4;
+            // 
+            // txtidcartelera
+            // 
+            this.txtidcartelera.Location = new System.Drawing.Point(143, 59);
+            this.txtidcartelera.Name = "txtidcartelera";
+            this.txtidcartelera.Size = new System.Drawing.Size(100, 20);
+            this.txtidcartelera.TabIndex = 5;
+            // 
+            // txtidpelicula
+            // 
+            this.txtidpelicula.Location = new System.Drawing.Point(143, 99);
+            this.txtidpelicula.Name = "txtidpelicula";
+            this.txtidpelicula.Size = new System.Drawing.Size(100, 20);
+            this.txtidpelicula.TabIndex = 6;
+            // 
+            // txtventastotales
+            // 
+            this.txtventastotales.Location = new System.Drawing.Point(143, 326);
+            this.txtventastotales.Name = "txtventastotales";
+            this.txtventastotales.Size = new System.Drawing.Size(100, 20);
+            this.txtventastotales.TabIndex = 7;
+            // 
+            // btnventfuncion
+            // 
+            this.btnventfuncion.Location = new System.Drawing.Point(293, 52);
+            this.btnventfuncion.Name = "btnventfuncion";
+            this.btnventfuncion.Size = new System.Drawing.Size(114, 23);
+            this.btnventfuncion.TabIndex = 8;
+            this.btnventfuncion.Text = "Ventas por Función";
+            this.btnventfuncion.UseVisualStyleBackColor = true;
+            // 
+            // btnventaspelicula
+            // 
+            this.btnventaspelicula.Location = new System.Drawing.Point(293, 97);
+            this.btnventaspelicula.Name = "btnventaspelicula";
+            this.btnventaspelicula.Size = new System.Drawing.Size(114, 23);
+            this.btnventaspelicula.TabIndex = 9;
+            this.btnventaspelicula.Text = "Ventas por Pelicula";
+            this.btnventaspelicula.UseVisualStyleBackColor = true;
+            // 
+            // btnventastotales
+            // 
+            this.btnventastotales.Location = new System.Drawing.Point(293, 142);
+            this.btnventastotales.Name = "btnventastotales";
+            this.btnventastotales.Size = new System.Drawing.Size(114, 23);
+            this.btnventastotales.TabIndex = 10;
+            this.btnventastotales.Text = "Total Ventas";
+            this.btnventastotales.UseVisualStyleBackColor = true;
+            // 
+            // btnventaslapso
+            // 
+            this.btnventaslapso.Location = new System.Drawing.Point(293, 191);
+            this.btnventaslapso.Name = "btnventaslapso";
+            this.btnventaslapso.Size = new System.Drawing.Size(114, 23);
+            this.btnventaslapso.TabIndex = 11;
+            this.btnventaslapso.Text = "Ventas por Lapso";
+            this.btnventaslapso.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Fecha de Inicio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Fecha Fin";
+            // 
+            // txtfechaini
+            // 
+            this.txtfechaini.Location = new System.Drawing.Point(143, 139);
+            this.txtfechaini.Name = "txtfechaini";
+            this.txtfechaini.Size = new System.Drawing.Size(100, 20);
+            this.txtfechaini.TabIndex = 14;
+            // 
+            // txtfechafin
+            // 
+            this.txtfechafin.Location = new System.Drawing.Point(143, 177);
+            this.txtfechafin.Name = "txtfechafin";
+            this.txtfechafin.Size = new System.Drawing.Size(100, 20);
+            this.txtfechafin.TabIndex = 15;
+            // 
+            // txttiquetes
+            // 
+            this.txttiquetes.Location = new System.Drawing.Point(143, 219);
+            this.txttiquetes.Name = "txttiquetes";
+            this.txttiquetes.Size = new System.Drawing.Size(100, 20);
+            this.txttiquetes.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Tiquetes Vendidos";
+            // 
+            // Reportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 372);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txttiquetes);
+            this.Controls.Add(this.txtfechafin);
+            this.Controls.Add(this.txtfechaini);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnventaslapso);
+            this.Controls.Add(this.btnventastotales);
+            this.Controls.Add(this.btnventaspelicula);
+            this.Controls.Add(this.btnventfuncion);
+            this.Controls.Add(this.txtventastotales);
+            this.Controls.Add(this.txtidpelicula);
+            this.Controls.Add(this.txtidcartelera);
+            this.Controls.Add(this.txtidsala);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Reportes";
             this.Text = "Reportes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtidsala;
+        private System.Windows.Forms.TextBox txtidcartelera;
+        private System.Windows.Forms.TextBox txtidpelicula;
+        private System.Windows.Forms.TextBox txtventastotales;
+        private System.Windows.Forms.Button btnventfuncion;
+        private System.Windows.Forms.Button btnventaspelicula;
+        private System.Windows.Forms.Button btnventastotales;
+        private System.Windows.Forms.Button btnventaslapso;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtfechaini;
+        private System.Windows.Forms.TextBox txtfechafin;
+        private System.Windows.Forms.TextBox txttiquetes;
+        private System.Windows.Forms.Label label7;
     }
 }
