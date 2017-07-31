e91f062ce0977f1892fc0f01e02968b9a8ff7cfa
Alfredo <Alfredo@VALVERDE>
2017-07-30 19:06:03 -0600

    Promiciones

--- ProyectoBD1/CapaDatos/Promociones.cs
+++ ProyectoBD1/CapaDatos/Promociones.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,60 @@ namespace CapaDatos
 {
     public class Promociones
     {
+        clsConexion conect = new clsConexion();
+        clsConexion consultar = new clsConexion();
+
+        public bool insertarPromociones(int idPromocion, string Nombre, int idPelicula, string Restricciones, string Fechaini, string Fechafin)
+        {
+            bool creandoPromociones;
+
+            creandoPromociones = conect.ejecutarInsert("insert into");
+
+            if (creandoPromociones)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public DataTable CargarPromociones(int idPromociones)
+        {
+            CapaDatos.clsConexion conecta = new CapaDatos.clsConexion();
+            DataTable dtPromociones;
+            dtPromociones = conecta.ejecutar("select * from Promociones where idPromocion = " + idPromociones);
+            return dtPromociones;
+        }
+
+        public DataTable CargarPromociones()
+        {
+            CapaDatos.clsConexion conecta = new CapaDatos.clsConexion();
+            DataTable dtPromociones;
+            dtPromociones = conecta.ejecutar("select * from Promociones");
+            return dtPromociones;
+        }
+
+        public bool actualizarPromociones(int idPromocion, string Nombre, int idPelicula, string Restricciones, string Fechaini, string Fechafin)
+        {
+            bool actualizandoPromociones;
+
+            actualizandoPromociones = conect.ejecutarInsert("update Cartelera set FK_Cartelera_Pelicula = ");
+            if (actualizandoPromociones)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool eliminarPromociones(int idPromociones)
+        {
+            bool eliminarPromociones;
+            eliminarPromociones = conect.ejecutarInsert("DELETE Promociones FROM Promociones where idPromocion = " + idPromociones + "");
+            if (eliminarPromociones)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
